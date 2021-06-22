a53ae3f8625825e9c65b7f8bd1166d2eafdbab6b
KoheiNishino <koheinishino.95@gmail.com>
2021-06-22 22:18:37 +0900

    07. Click on the movie thumbnail to view the trailer.

--- src/components/Row.tsx
+++ src/components/Row.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import YouTube from "react-youtube";
 import axios from "../axios";
 import "./Row.scss";
 
@@ -19,9 +20,20 @@ type Movie = {
   backdrop_path: string;
 };
 
+//trailerのoption
+type Options = {
+  height: string;
+  width: string;
+  playerVars: {
+    autoplay: 0 | 1 | undefined;
+  };
+};
+
 export const Row = ({ title, fetchUrl, isLargeRow }: Props) => {
   const [movies, setMovies] = useState<Movie[]>([]);
+  const [trailerUrl, setTrailerUrl] = useState<string | null>("");
 
+  //urlが更新される度に
   useEffect(() => {
     async function fetchData() {
       const request = await axios.get(fetchUrl);
@@ -30,6 +42,25 @@ export const Row = ({ title, fetchUrl, isLargeRow }: Props) => {
     }
     fetchData();
   }, [fetchUrl]);
+
+  const opts: Options = {
+    height: "390",
+    width: "640",
+    playerVars: {
+      // https://developers.google.com/youtube/player_parameters
+      autoplay: 1,
+    },
+  };
+
+  const handleClick = async (movie: Movie) => {
+    if (trailerUrl) {
+      setTrailerUrl("");
+    } else {
+      const API_KEY = process.env.REACT_APP_TMDB_API_KEY;
+      let trailerurl = await axios.get(`/movie/${movie.id}/videos?api_key=${API_KEY}`);
+      setTrailerUrl(trailerurl.data.results[0]?.key);
+    }
+  };
 
   return (
     <div className="Row">
@@ -44,9 +75,11 @@ export const Row = ({ title, fetchUrl, isLargeRow }: Props) => {
               isLargeRow ? movie.poster_path : movie.backdrop_path
             }`}
             alt={movie.name}
+            onClick={() => handleClick(movie)}
           />
         ))}
       </div>
+      {trailerUrl && <YouTube videoId={trailerUrl} opts={opts} />}
     </div>
   );
 };
