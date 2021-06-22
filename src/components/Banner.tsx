b5da0fd9ad5a6c49435b97e9e3ab574b3cd243f2
KoheiNishino <koheinishino.95@gmail.com>
2021-06-22 22:05:06 +0900

    06. Create a Nav component.

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,7 +1,7 @@
 import axios from "../axios";
 import { useEffect, useState } from "react";
 import { requests } from "../request";
-import "./Banner.scss"
+import "./Banner.scss";
 
 type movieProps = {
   title?: string;
