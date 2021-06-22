81ac4716f1d48ccc1f546527c017cf00ce7c1372
KoheiNishino <koheinishino.95@gmail.com>
2021-06-22 21:44:14 +0900

    04. Create a Row component.

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,21 @@
-import React from 'react';
 import './App.css';
+import { requests } from './request';
+import { Row } from './components/Row';
 
 function App() {
   return (
     <div className="App">
+      <Row
+        title="Netflix Originals"
+        fetchUrl={requests.feachNetflixOriginals}
+        isLargeRow
+      />
+      <Row title="Top Rated" fetchUrl={requests.feactTopRated} />
+      <Row title="Action Movies" fetchUrl={requests.feactActionMovies} />
+      <Row title="Comedy Movies" fetchUrl={requests.feactComedyMovies} />
+      <Row title="Horror Movies" fetchUrl={requests.feactHorrorMovies} />
+      <Row title="Romance Movies" fetchUrl={requests.feactRomanceMovies} />
+      <Row title="DOcumentaries" fetchUrl={requests.feactDocumentMovies} />
     </div>
   );
 }
